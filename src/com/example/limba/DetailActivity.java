b2bf2267c67db3e9125d608d1874d856a616cb32
LitWit <cancino@tzi.de>
2014-07-03 21:04:13 +0200

    Microphone
    Wir haben schon mal probiert wie man das anbaut.

--- src/com/example/limba/DetailActivity.java
+++ src/com/example/limba/DetailActivity.java
@@ -1,18 +1,31 @@
 package com.example.limba;
 
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class DetailActivity extends Activity {
+	
+	private static ImageButton microButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.vokabeldetail_neu);
-
+		
+		microButton = (ImageButton) findViewById(R.id.button1);
+		
+		
 		Intent intent = getIntent();
 
 		int position = (int) intent.getIntExtra("position", 0);
@@ -25,7 +38,23 @@ public class DetailActivity extends Activity {
 
 		((TextView) (findViewById(R.id.editText1))).setText(vokabel
 				.getNext().getPersischeVokabel());
-		((TextView) (findViewById(R.id.persischesTextfeld))).setText(vokabel
-				.getNext().getPersischeVokabel());
-	}
+		/*((TextView) (findViewById(R.id.persischesTextfeld))).setText(vokabel
+				.getNext().getPersischeVokabel()); */
+		
+		microButton.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				MediaRecorder micro = new MediaRecorder();
+				micro.setAudioSource(MediaRecorder.AudioSource.MIC);
+				micro.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+				//FileOutputStream f = 
+//				getResources().openRawResource(R.);
+				
+				
+			}
+		});
+		
+		}
 }
