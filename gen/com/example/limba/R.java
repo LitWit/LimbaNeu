51bc3ba1e419d9ebe5cadcf38bac9e304b059124
LitWit <cancino@tzi.de>
2014-07-06 19:35:05 +0200

    Viele Veränderungen überall

--- gen/com/example/limba/R.java
+++ gen/com/example/limba/R.java
@@ -1298,22 +1298,16 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050027;
         public static final int action_menu_presenter=0x7f050028;
         public static final int action_mode_close_button=0x7f050035;
-        public static final int action_settings=0x7f050062;
+        public static final int action_settings=0x7f050060;
         public static final int activity_chooser_view_content=0x7f050036;
         public static final int alignBounds=0x7f050002;
         public static final int alignMargins=0x7f050003;
         public static final int always=0x7f05001d;
         public static final int beginning=0x7f050023;
         public static final int bewerten_button=0x7f050051;
-        public static final int bildButton=0x7f05005f;
+        public static final int bildButton=0x7f05005d;
         public static final int bildbutton=0x7f050054;
         public static final int bottom=0x7f050005;
-        public static final int button1=0x7f050057;
-        public static final int button2=0x7f050058;
-        public static final int button3=0x7f05005b;
-        public static final int button4=0x7f05005c;
-        public static final int button5=0x7f05005d;
-        public static final int button6=0x7f05005e;
         public static final int center=0x7f05000c;
         public static final int center_horizontal=0x7f05000a;
         public static final int center_vertical=0x7f050008;
@@ -1323,11 +1317,10 @@ containing a value of this type.
         public static final int collapseActionView=0x7f05001f;
         public static final int container=0x7f05004d;
         public static final int default_activity_button=0x7f050039;
+        public static final int deutschesTextfeld=0x7f050058;
         public static final int dialog=0x7f050020;
         public static final int disableHome=0x7f05001a;
         public static final int dropdown=0x7f050021;
-        public static final int editText1=0x7f05005a;
-        public static final int editText2=0x7f050061;
         public static final int edit_query=0x7f050041;
         public static final int end=0x7f050011;
         public static final int expand_activities_button=0x7f050037;
@@ -1341,16 +1334,21 @@ containing a value of this type.
         public static final int icon=0x7f05003b;
         public static final int ifRoom=0x7f05001c;
         public static final int image=0x7f050038;
+        public static final int lautsprecherDeutschButton=0x7f05005a;
+        public static final int lautsprecherPersischButton=0x7f050057;
         public static final int left=0x7f050006;
         public static final int listMode=0x7f050013;
         public static final int list_item=0x7f05003a;
+        public static final int microDeutschButton=0x7f050059;
+        public static final int microPersischButton=0x7f050056;
         public static final int middle=0x7f050024;
         public static final int neue_woerter_button=0x7f05004f;
         public static final int neue_woerter_liste=0x7f050053;
         public static final int never=0x7f05001b;
         public static final int none=0x7f050022;
         public static final int normal=0x7f050012;
-        public static final int persischesTextfeld=0x7f050060;
+        public static final int persischesTextfeld=0x7f050055;
+        public static final int profilbildButton=0x7f05005e;
         public static final int progress_circular=0x7f050029;
         public static final int progress_horizontal=0x7f05002a;
         public static final int radio=0x7f050040;
@@ -1373,17 +1371,17 @@ containing a value of this type.
         public static final int start=0x7f050010;
         public static final int submit_area=0x7f05004a;
         public static final int tabMode=0x7f050014;
-        public static final int textView1=0x7f050055;
-        public static final int textView2=0x7f050059;
-        public static final int textView3=0x7f050056;
         public static final int title=0x7f05003c;
         public static final int top=0x7f050004;
         public static final int top_action_bar=0x7f050031;
+        public static final int trainierenButton=0x7f05005c;
         public static final int up=0x7f050032;
         public static final int useLogo=0x7f050015;
         public static final int vertical=0x7f050001;
+        public static final int vokabellisteHinzufuegenButton=0x7f05005b;
         public static final int vokabelliste_button=0x7f050050;
         public static final int withText=0x7f05001e;
+        public static final int wortHinzufuegenButton=0x7f05005f;
         public static final int wort_hinzufuegen_button=0x7f050052;
     }
     public static final class integer {
@@ -1440,12 +1438,13 @@ containing a value of this type.
         public static final int mainboard=0x7f030019;
         public static final int neue_woerter=0x7f03001a;
         public static final int support_simple_spinner_dropdown_item=0x7f03001b;
-        public static final int vokabeldetail=0x7f03001c;
-        public static final int vokabeldetail_neu=0x7f03001d;
+        public static final int trainieren=0x7f03001c;
+        public static final int vokabeldetail=0x7f03001d;
         public static final int wort_hinzufuegen=0x7f03001e;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
+        public static final int trainieren=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1491,6 +1490,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int title_activity_trainieren=0x7f0a0010;
     }
     public static final class style {
         /** 
